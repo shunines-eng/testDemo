97c586c90955342ed4995d36e661fc2c6c0c0dd4
shunines <3298978149@qq.com>
2025-05-17 13:49:28 +0800

    再次用aider更改了代码

---
+++ src/main null.py
@@ -0,0 +1,9 @@
+class LoginController:
+    """控制界面的登录验证逻辑"""
+    
+    def __main__(self):
+        # 主调 login_service 的函数
+        self.run()
+
+    def __init__(self):
+        self.service = service_name()

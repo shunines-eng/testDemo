170af7377e1d99e77a8cf4b5bc529bb1b67b68da
shunines <3298978149@qq.com>
2025-05-17 14:25:43 +0800

    给LoginService.py加一些注释吧

--- src/main null.py
+++
@@ -1,9 +0,0 @@
-class LoginController:
-    """控制界面的登录验证逻辑"""
-    
-    def __main__(self):
-        # 主调 login_service 的函数
-        self.run()
-
-    def __init__(self):
-        self.service = service_name()
